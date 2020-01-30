29b12743654119dc62402c7487a874a26bb78fa0
LargeMan <50443291+LargeMan@users.noreply.github.com>
2020-01-29 19:10:10 -0500

    Sobel v2
    Improved runtime dramatically by using more lists

--- Sobel.hs
+++ Sobel.hs
@@ -2,13 +2,9 @@
 Sobel implementation in Haskell.
 Meant for 256 by 256 PGM images;
 however, can probably be adjusted for other sizes.
-
-Needs some improvement, particularly in runtime.
 -}
 
-
-
-module Sobel where
+--module Sobel where
 
 import Data.Word
 import Data.List
@@ -29,7 +25,7 @@ maskY = [[ 1, 2, 1], [ 0, 0, 0], [-1,-2,-1]]
 header :: [Word8]
 header = [80, 53, 10, 50, 53, 54, 32, 50, 53, 54, 10, 50, 51, 53, 10]
 
--- Input and Output via command line args
+{- Input and Output via command line args -}
 main = do
     args <- getArgs
     fp1 <- openFile (args !! 0) ReadMode
@@ -39,52 +35,76 @@ main = do
     --let lo  = (args !! 4)
     --let hi  = (args !! 5)
     fpbl <- BL.hGetContents fp1
-    let pic = removeHeader $ BL.unpack fpbl
-    let mags = sums 256 (toList pic)
-    let out1 = header ++ (norm ((maximum mags)*1.25) 256 mags)
+    let (_, pic) = splitAt 15 (BL.unpack fpbl)
+    let (a, b, c) = toList 256 pic
+    let top = (replicate 256 0) ++ a
+    let bot = c ++ (replicate 256 0)
+    let xs = zip9   ((tail top) ++ [0]) (top) (0:top)
+                    ((tail b)   ++ [0]) (b)   (0:b)
+                    ((tail bot) ++ [0]) (bot) (0:bot)
+    let mags = sums 256 xs
+    let out1 = header ++ (norm ((maximum mags)*1.15) 256 mags)
     BL.hPut fo1 (BL.pack out1)
     
 
-removeHeader (_:_:_:_:_:_:_:_:_:_:_:_:_:_:_:xs) = xs
+{- Create three lists (-n, base, +n) -}
+toList :: Int -> [Word8] -> ([Float], [Float], [Float])
+toList n xs = (a, b, c) where
+    b = [(fromIntegral(x) :: Float) | x <- xs]
+    (_,c) = splitAt n b       -- +n
+    (a,_) = splitAt (n^2-n) b -- -n
 
-toList :: [Word8] -> [Int]
-toList xs = [(fromIntegral(x) :: Int) | x <- xs]
+
+{- Zips up 9 lists -}
+zip9 :: [Float] -> [Float] -> [Float] -> 
+        [Float] -> [Float] -> [Float] -> 
+        [Float] -> [Float] -> [Float] ->  [(Float, Float, Float,
+                                            Float, Float, Float,
+                                            Float, Float, Float)]
+zip9 (a:as) (b:bs) (c:cs)
+     (d:ds) (e:es) (f:fs)
+     (g:gs) (h:hs) (i:is)
+        = (a,b,c,d,e,f,g,h,i) : zip9 as bs cs ds es fs gs hs is
+zip9 _ _ _ _ _ _ _ _ _ = []
+
 
 {- Calculate the weighted sums for each mask,
     then convolutes them to find magnitude.
     Automatically sets sums at borders to 0 -}
-sums :: Int -> [Int] -> [Float]
-sums n xs =
-    [z | i <- [0..(n^2 - 1)],
-        let (a,b) = if (i `mod` n == 0     ||
-                        (i+1) `mod` n == 0)||
-                        i   <   n          ||
-                        i > ((n * (n-1))-1)  then (0, 0)
-                    else
-                      ((
-                        (xs !! (i-n-1)) * ((maskX !! 0) !! 0) +
-                        (xs !! (i-n) )  * ((maskX !! 0) !! 1) +
-                        (xs !! (i-n+1)) * ((maskX !! 0) !! 2) +
-                        (xs !! (i-1))   * ((maskX !! 1) !! 0) +
-                        (xs !!   i  )   * ((maskX !! 1) !! 1) +
-                        (xs !! (i+1))   * ((maskX !! 1) !! 2) +
-                        (xs !! (i+n-1)) * ((maskX !! 2) !! 0) +
-                        (xs !! (i+n))   * ((maskX !! 2) !! 1) +
-                        (xs !! (i+n+1)) * ((maskX !! 2) !! 2)
-                        ),
+
+sums :: Int -> [(Float, Float, Float,
+                 Float, Float, Float,
+                 Float, Float, Float)] -> [Float]
+
+sums n xs
+    = [z | 
+           (i, (tl,t,tr,l,c,r,bl,b,br)) <- (zip [0..(n^2 - 1)] xs),
+           let (x,y) = if (i `mod` n == 0
+                          || (i+1) `mod` n == 0)
+                          || i   <   n 
+                          || i > ((n * (n-1))-1) then (0, 0)
+                       else
                         (
-                        (xs !! (i-n-1)) * ((maskY !! 0) !! 0) +
-                        (xs !! (i-n) )  * ((maskY !! 0) !! 1) +
-                        (xs !! (i-n+1)) * ((maskY !! 0) !! 2) +
-                        (xs !! (i-1))   * ((maskY !! 1) !! 0) +
-                        (xs !!   i  )   * ((maskY !! 1) !! 1) +
-                        (xs !! (i+1))   * ((maskY !! 1) !! 2) +
-                        (xs !! (i+n-1)) * ((maskY !! 2) !! 0) +
-                        (xs !! (i+n))   * ((maskY !! 2) !! 1) +
-                        (xs !! (i+n+1)) * ((maskY !! 2) !! 2)
-                        )
-                      ),
-          let z = sqrt (fromIntegral (a^2 + b^2) :: Float)]
+                          (
+                            (tl * (-1)) +
+                            (tr)        +
+                            (l  * (-2)) +
+                            (r  * 2)    +
+                            (bl * (-1)) +
+                            (br)
+                          ),
+                          (
+                            (tl)        +
+                            (t  * 2)    +
+                            (tr)        +
+                            (bl * (-1)) +
+                            (b  * (-2)) +
+                            (br * (-1))
+                          )
+                        ),
+
+           let z = sqrt (x**2 + y**2)]
+
 
 -- Normalizes magnitude values and converts to Word8
 norm :: Float -> Float -> [Float] -> [Word8]
